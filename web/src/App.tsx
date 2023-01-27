e9222d41d9a9c6318ff6907c8f3c83e8b34ae546
Leonan Costa <leonanfecosta@gmail.com>
2023-01-27 17:13:56 -0300

    feat: componetiza o header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,13 @@
 // import { Habit } from "./components/Habit";
 import './styles/global.css'
-import logoImage from './assets/logo.svg'
-import { Plus } from 'phosphor-react'
+import { Header } from './components/Header';
+
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src={logoImage} alt="Habits" />
-
-          <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-          >
-            <Plus size={20} className="text-violet-500" />
-            Novo Hábito
-          </button>
-        </div>
+        <Header />
       </div>
     </div>
   );
